d32ceca9b8ec19e31cd5da7f043b8e1cc15d8115
PotatoAim20 <ns.darwish1@gmail.com>
2025-08-24 14:46:33 +0300

    !

--- lib/screens/registeration/login_screen.dart
+++ lib/screens/registeration/login_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:movies_app/widgets/custom_text_form_field.dart';
 
 class LoginScreen extends StatelessWidget {
